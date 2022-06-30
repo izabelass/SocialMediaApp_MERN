068126450ae69df52fa20020c437e40fb2ef7de1
Izabela Soares <soares.izabela96@gmail.com>
2022-06-30 20:38:31 -0300

    setting up manual auth, sign in and sign up

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -8,23 +8,33 @@ import Icon from './icon';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
 import Input from './Input';
+import { signin, signup } from '../../actions/auth';
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' }
 
 const Auth = () => {
     const classes = useStyles();
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignup] = useState(false);
+    const [formData, setFormData] = useState(initialState);
     const dispatch = useDispatch();
     const history = useHistory();
 
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword); //if its on turn it off and vice-versa
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if(isSignup) {
+            dispatch(signup(formData, history))
+        } else {
+            dispatch(signin(formData, history))
+        }
     };
 
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value})
     };
 
     const switchMode = () => {
